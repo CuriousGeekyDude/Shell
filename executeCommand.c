12fad526e465580fb87d009b69d714218e8c2543
CuriousGeekyDude <farzadaghnoom@gmail.com>
2023-06-21 12:53:53 +0200

    Include the necessary headers in .c not .h

---
+++ executeCommand.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include "builtInC.h"
+#include "nonBuiltInC.h"
+#include "executeCommand.h"
+
+
+
+pid_t executeCommand(char* argv, size_t begIndexOfCommand, size_t numOfStringsInCommand, bool toFork)
+{
+    if(strcmp(argv[begIndexOfCommand], "cd") == 0) {
+        if(cdCommand(begIndexOfCommand, numOfStringsInCommand) == -1)
+            return -1;
+        return 0;
+    }
+
+    if(strcmp(argv[begIndexOfCommand], "exit") == 0) {
+        exitCommand(numOfStringsInCommand);
+        return -1;
+    }
+
+    if(strcmp(argv[begIndexOfCommand], "exec") == 0) {
+        execCommand(begIndexOfCommand, numOfStringsInCommand);
+        return 0;
+    }
+    
+    if(strcmp(argv[begIndexOfCommand], "pwd") == 0) {
+        pwdCommand(numOfStringsInCommand);
+        return 0;
+    }
+        
+
+    if(toFork == true)
+        return nonBuiltInCommand(begIndexOfCommand, numOfStringsInCommand);
+
+    else
+        exec_nonBuiltInCommand(begIndexOfCommand, numOfStringsInCommand);
+}
---
+++ executeCommand.h
@@ -0,0 +1,9 @@
+#ifdef EXECUTE_COMMAND_H
+#define EXECUTE_COMMAND_H
+
+#include <stdbool.h>
+
+pid_t executeCommand(char* argv, size_t begIndexOfCommand, size_t numOfStringsInCommand, bool toFork);
+
+
+#endif
