049cd4951f0296ee425c371f391982770ca0d3af
CuriousGeekyDude <farzadaghnoom@gmail.com>
2023-06-21 12:57:41 +0200

    Include necessary headers in .c files

--- executeCommandBlock.c
+++ executeCommandBlock.c
@@ -1,38 +1,9 @@
+#include <string.h>
+#include "pipe_redirection.h"
 #include "executeCommand.h"
+#include "executeCommandBlock.h"
 
-
-pid_t executeCommand(size_t begIndexOfCommand, size_t numOfStringsInCommand, bool toFork)
-{
-    if(strcmp(argv[begIndexOfCommand], "cd") == 0) {
-        if(cdCommand(begIndexOfCommand, numOfStringsInCommand) == -1)
-            return -1;
-        return 0;
-    }
-
-    if(strcmp(argv[begIndexOfCommand], "exit") == 0) {
-        exitCommand(numOfStringsInCommand);
-        return -1;
-    }
-
-    if(strcmp(argv[begIndexOfCommand], "exec") == 0) {
-        execCommand(begIndexOfCommand, numOfStringsInCommand);
-        return 0;
-    }
-    
-    if(strcmp(argv[begIndexOfCommand], "pwd") == 0) {
-        pwdCommand(numOfStringsInCommand);
-        return 0;
-    }
-        
-
-    if(toFork == true)
-        return nonBuiltInCommand(begIndexOfCommand, numOfStringsInCommand);
-
-    else
-        exec_nonBuiltInCommand(begIndexOfCommand, numOfStringsInCommand);
-}
-
-pid_t executeCommandBlock(struct CommandBlock* commandBlock)
+pid_t executeCommandBlock(struct CommandBlock* commandBlock, char* argv)
 {
     pid_t childPID = -1;
 
--- executeCommandBlock.h
+++ executeCommandBlock.h
@@ -1,13 +1,8 @@
 #ifndef EXECUTECOMMAND_H
 #define EXECUTECOMMAND_H
 
-#include "basic.h"
 #include "commandBlock.h"
-#include "builtInC.h"
-#include "nonBuiltInC.h"
 
-
-pid_t executeCommand(size_t begIndexOfCommand, size_t numOfStringsInCommand, bool toFork);
-pid_t executeCommandBlock(struct CommandBlock* commandBlock);
+pid_t executeCommandBlock(struct CommandBlock* commandBlock, char* argv);
 
 #endif
