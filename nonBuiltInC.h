80fe2c02eb8bd0831beae67720f1ae1ceff4d449
CuriousGeekyDude <farzadaghnoom@gmail.com>
2023-06-22 12:13:21 +0200

    Add sys/types.h for in order to be able to use pid_t

--- nonBuiltInC.h
+++ nonBuiltInC.h
@@ -1,6 +1,7 @@
 #ifndef NONBUILTINC_H
 #define NONBUILTINC_H
 
+#include <sys/types.h>
 
 void exec_nonBuiltInCommand(size_t indexArgv, size_t argc, char** argv);
 pid_t nonBuiltInCommand(size_t indexArgv, size_t argc, char** argv);
