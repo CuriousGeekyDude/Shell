5d76f9cb79e603b3aa9e7ba44fabc0e2926888bb
CuriousGeekyDude <farzadaghnoom@gmail.com>
2023-06-22 12:14:45 +0200

    Add sys/types.h in order to be able to use pid_t

--- executeCommand.h
+++ executeCommand.h
@@ -2,6 +2,8 @@
 #define EXECUTE_COMMAND_H
 
 #include <stdbool.h>
+#include <sys/types.h>
+
 
 pid_t executeCommand(char** argv, size_t begIndexOfCommand, size_t numOfStringsInCommand, bool toFork);
 
