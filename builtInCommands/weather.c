4ea1491ac7aa31718f68c304ac91dff8075a7c17
CuriousGeekyDude <farzadaghnoom@gmail.com>
2023-06-25 05:02:55 +0200

    Add one more argument to execlp()

--- builtInCommands/weather.c
+++ builtInCommands/weather.c
@@ -11,24 +11,13 @@ pid_t weatherCommand(size_t begIndexOfCommand, size_t numOfStringsInCommand, con
 {
     if(numOfStringsInCommand > 3 || numOfStringsInCommand < 3)
         return -1;
-    
-
-    char* cityArgument = NULL;
-    char* countryArgument = NULL;
-
-    if(sprintf(cityArgument, "%s", argv[1]) < 0) {
-        errExit("sprintf()1 in weatherCommand");
-    }
-    if(sprintf(countryArgument, "%s", argv[2]) < 0) {
-        errExit("sprintf()2 in weatherCommand");
-    }
 
     pid_t childPID = -1;
     if(strcmp("N", commandType) == 0) {
 
         switch(fork()) {
             case 0:
-                if(execlp("python3", "weather.py", "--city", cityArgument, "--country", countryArgument, NULL) == -1) {
+                if(execlp("python3", "python3","weather.py", "--city", argv[1], "--country", argv[2], NULL) == -1) {
                     errExit("execlp");
                 }
                 break;
@@ -42,7 +31,7 @@ pid_t weatherCommand(size_t begIndexOfCommand, size_t numOfStringsInCommand, con
 
     else {
 
-        if(execlp("python3", "weather.py", "--city", cityArgument, "--country", countryArgument, NULL) == -1) {
+        if(execlp("python3", "python3" ,"weather.py", "--city", argv[1], "--country", argv[2], NULL) == -1) {
                     errExit("execlp");
         }
     }
