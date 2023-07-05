3cff1bd5d6f3d55fa17f7454bf89426cd2c7060c
NicolasbourbakII <farzadaghnoom@gmail.com>
2023-07-05 13:54:36 +0200

    Add exception handling in main for Terminal instance

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <exception>
+#include <system_error>
+#include <string>
+#include <iostream> 
+#include <Shell_Terminal.hpp>
+
+extern "C" {
+    #include <basic.h>
+    #include <history.h>
+    #include <shell_start.h>
+}
+
+
+void signalHandler(int signal)
+{
+/*
+    if(signal == SIGTSTP) {
+        if(currentHistoryBlock == NULL)
+            return;
+        if(currentHistoryBlock->command == NULL) {
+            printf("\nnull!\n");
+            fflush(stdout);
+        }
+        else {
+            printf("\n%s\n", currentHistoryBlock->command);
+            fflush(stdout);
+        }
+        if(currentHistoryBlock->next == NULL) {
+            printShellSign();
+            return;
+        }
+        currentHistoryBlock = currentHistoryBlock->next;
+        printShellSign();
+
+    }
+    */
+}
+
+
+
+
+
+
+
+int main(void)
+{
+    struct sigaction sig;
+    memset(&sig, 0, sizeof(struct sigaction));
+    sig.sa_handler = &signalHandler;
+    sig.sa_flags = SA_RESTART;
+
+    sigaction(SIGTSTP, &sig, NULL);
+
+    try {
+        Shell_Terminal binder;
+        while(1) {
+            binder.terminal.enterRawMode();
+            binder.terminal.processKeyPress();
+            binder.transferInputTo_InputWords();
+            binder.terminal.disableRawMode();
+            binder.terminal.emptyInput();
+            start_shell();
+            binder.transferHistoryTo_Terminal();
+        }
+    }
+
+    catch(std::system_error& error) {
+        std::cerr << "Error1: " << error.code().message() << ".\nAborting..." << std::endl;
+        return -1;            
+    }
+    catch(std::string& error) {
+        std::cerr << "Error2: " << error << "\nAborting..." << std::endl;
+    }
+
+    freeAllBlocks();
+    exit(EXIT_SUCCESS);
+}
